64676dd40592649ea02b21ad8ac21c099953884c
derkach2288 <derkach2288@gmail.com>
2023-11-28 12:22:34 +0100

    add lesson 19

--- src/lessons/Lesson19/Lesson19.jsx
+++ src/lessons/Lesson19/Lesson19.jsx
@@ -1,18 +1,37 @@
 import "./styles.css";
 import { useState } from "react";
 import Button from "../../components/Button";
+import Counter from "../../components/Counter/Counter";
 
 function Lesson19() {
-
   const [item, setItem] = useState(["apple"]);
+
+  const [person, setPerson] = useState({ name: "Kate", age: 28 });
+
+  const [name, setName] = useState();
+  console.log(name);
 
   const addFruites = (newFruitsArray) => {
     setItem((prevValue) => [...prevValue, ...newFruitsArray]);
-  }
+  };
+
+  const addPesonHairColor = (colorHair) => {
+    setPerson((prevValue) => ({ ...prevValue, colorHair }));
+  };
 
   const newItems = item.map((el) => {
-    return <div key={`fruits-items-${Math.random()}`}>{el}</div>
-  })
+    return <div key={`fruits-items-${Math.random()}`}>{el}</div>;
+  });
+
+  const newPersons = Object.keys(person).map((personKeys) => {
+    return (
+      <li key={`person-props-${Math.random()}`}>
+        {personKeys}: {person[personKeys]}
+      </li>
+    );
+  });
+  console.log(newPersons);
+
   return (
     <div className="lesson19-wrapper">
       <div className="itemsControl">{newItems}</div>
@@ -22,6 +41,26 @@ function Lesson19() {
           onClick={() => addFruites(["chery", "orange"])}
         />
       </div>
+      <div className="itemsControl">
+        Person priperies:
+        <ul>{newPersons}</ul>
+      </div>
+      <div className="buttonControl">
+        <Button
+          name="Add Hair Color"
+          onClick={() => addPesonHairColor("black")}
+        />
+      </div>
+      <div>
+        <Counter />
+      </div>
+      <div className="buttonControl">
+        <Button
+          name="Add Name"
+          onClick={() => setName("Alex")}
+        />
+      </div>
+      {name}
     </div>
   );
 }
